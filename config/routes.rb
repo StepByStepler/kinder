22eb304b8bdedb31bbfb06dad9ef768a580bd42f
Grigory Panko <gpanko@bk.ru>
2021-12-12 00:12:33 +0300

    Add datings/update path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/register'
   get 'sessions/confirm'
   get 'datings/view'
+  post 'datings/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
