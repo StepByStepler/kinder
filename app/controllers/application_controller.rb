ab0146a92a8d443c1b3645f9abcab2dd25251e19
Grigory Panko <gpanko@bk.ru>
2021-12-11 21:19:00 +0300

    Do not allow using DatingsController if user is not authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
+# Base class for all application controllers
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&action)
+    locale_cookie = cookies[:locale]&.to_sym
+    locale = I18n.locale_available?(locale_cookie) ? locale_cookie : I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  protected
+
+  def current_user
+    @current_user ||= session[:current_user_id] && User.find_by_id(session[:current_user_id])
+  end
+
+  def authenticate
+    redirect_to root_path if current_user.nil? || !current_user.email_confirmed
+  end
 end
--- app/controllers/datings_controller.rb
+++ app/controllers/datings_controller.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+# Controller which handles everything when user is already logged in
 class DatingsController < ApplicationController
+  before_action :authenticate
+
   def view
   end
 end
