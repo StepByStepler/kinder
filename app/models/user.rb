bcc2e3f3b92ae67672592465b3c0417832eebdb9
Grigory Panko <gpanko@bk.ru>
2021-12-12 20:30:07 +0300

    Validate that user age is positive, add method to check if user has profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :email
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :age, numericality: { greater_than_or_equal_to: 1 }, allow_nil: true
 
   before_create :apply_confirmation_token
 
@@ -12,6 +13,14 @@ class User < ApplicationRecord
     user = find_by_email(email)
     return nil if user.nil?
     return user if user.authenticate(submitted_password)
+  end
+
+  def profile_pic?
+    resource_path = File.join('images', 'profiles', "#{id}.png")
+    file_path = File.join('public', resource_path)
+    puts File.join('public', resource_path)
+    puts File.exist? file_path
+    File.exist? file_path
   end
 
   private
